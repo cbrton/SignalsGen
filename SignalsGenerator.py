055b583dd68efb200917ff6e3f3c35c9963a37a3
cbrton <cbrton0@yandex.ru>
2025-09-16 14:06:08 +0500

    Функции signal_matches_valve_di(), signal_matches_valve_do(), signal_matches_mtr_di() и signal_matches_mtr_do() объединены в функцию signal_matches() с входными переменными equipment_type и signal_type.

--- SignalsGenerator.py
+++ SignalsGenerator.py
@@ -1,125 +1,79 @@
 import re
-import csv
 import pandas as pd
 
-def signals_matches_valve_di(path: str) -> tuple:
+def signals_matches(path: str, equipment_type: str, signal_type: str) -> tuple:
     result = ''
     with open(path, 'r', encoding='utf-8') as file:
         var_declarations_string = file.read()
     declarations = var_declarations_string.split('\n')
     sr = declarations[0].split(sep=' ')[1]
 
-    for declaration in declarations:
-        object_name_list = re.search(r'\d{1,2}[a-z][a-z][a-z][a-z]?\d{1,3}(_\d)?', declaration)
-        object_name:str = ''
-        var_name:str = ''
-        signal_type:str = ''
-        if object_name_list:
-            object_name = object_name_list[0]
-            splitted_declaration = declaration.split(sep=':')
-            var_name = re.sub(r'\s', '', splitted_declaration[0])
-            splitted_var_name = var_name.split(sep='_')
-            signal_type = splitted_var_name[len(splitted_var_name) - 1]
+    if equipment_type == 'valve':
+        for declaration in declarations:
+            object_name_list = re.search(r'\d{1,2}[a-z][a-z][a-z][a-z]?\d{1,3}(_\d)?', declaration)
+            object_name: str = ''
+            var_name: str = ''
+            signal: str = ''
+            if object_name_list:
+                object_name = object_name_list[0]
+                splitted_declaration = declaration.split(sep=':')
+                var_name = re.sub(r'\s', '', splitted_declaration[0])
+                splitted_var_name = var_name.split(sep='_')
+                signal = splitted_var_name[len(splitted_var_name) - 1]
 
-        if object_name != '':
-            if signal_type == 'OPENED':
-                valve_opened_signal = 'V_' + object_name + '_opened' + '\t' + sr + '.' + var_name + '\n'
-                result += valve_opened_signal
+            if signal_type == 'di':
+                if object_name != '':
+                    if signal == 'OPENED':
+                        valve_opened_signal = 'V_' + object_name + '_opened' + '\t' + sr + '.' + var_name + '\n'
+                        result += valve_opened_signal
 
-            if signal_type == 'CLOSED':
-                valve_closed_signal = 'V_' + object_name + '_closed' + '\t' + sr + '.' + var_name + '\n'
-                result += valve_closed_signal
+                    if signal == 'CLOSED':
+                        valve_closed_signal = 'V_' + object_name + '_closed' + '\t' + sr + '.' + var_name + '\n'
+                        result += valve_closed_signal
 
-            if signal_type == 'Internal':
-                valve_fault_signal = 'V_' + object_name + '_fault' + '\t' + sr + '.' + var_name + '\n'
-                result += valve_fault_signal
-    return (result, sr)
+                    if signal == 'Internal':
+                        valve_fault_signal = 'V_' + object_name + '_fault' + '\t' + sr + '.' + var_name + '\n'
+                        result += valve_fault_signal
+            elif signal_type == 'do':
+                if object_name != '':
+                    if signal == 'OPEN':
+                        valve_opened_signal = 'V_' + object_name + '_open' + '\t' + sr + '.' + var_name + '\n'
+                        result += valve_opened_signal
 
-def signals_matches_valve_do(path: str) -> tuple:
-    result = ''
-    with open(path, 'r', encoding='utf-8') as file:
-        var_declarations_string = file.read()
-    declarations = var_declarations_string.split('\n')
-    sr = declarations[0].split(sep=' ')[1]
+                    if signal == 'CLOSE':
+                        valve_closed_signal = 'V_' + object_name + '_close' + '\t' + sr + '.' + var_name + '\n'
+                        result += valve_closed_signal
+    elif equipment_type == 'mtr':
+        for declaration in declarations:
+            object_name_list = re.search(r'N[A-Z]?\d{1,2}(_\d)?', declaration)
+            object_name: str = ''
+            var_name: str = ''
+            signal: str = ''
+            if object_name_list:
+                object_name = object_name_list[0]
+                splitted_declaration = declaration.split(sep=':')
+                var_name = re.sub(r'\s', '', splitted_declaration[0])
+                splitted_var_name = var_name.split(sep='_')
+                signal = splitted_var_name[len(splitted_var_name) - 1]
 
-    for declaration in declarations:
-        object_name_list = re.search(r'\d{1,2}[a-z][a-z][a-z][a-z]?\d{1,3}(_\d)?', declaration)
-        object_name:str = ''
-        var_name:str = ''
-        signal_type:str = ''
-        if object_name_list:
-            object_name = object_name_list[0]
-            splitted_declaration = declaration.split(sep=':')
-            var_name = re.sub(r'\s', '', splitted_declaration[0])
-            splitted_var_name = var_name.split(sep='_')
-            signal_type = splitted_var_name[len(splitted_var_name) - 1]
+            if signal_type == 'di':
+                if object_name != '':
+                    if signal == 'WORK':
+                        pump_work_signal = object_name + '_pump_work' + '\t' + sr + '.' + var_name + '\n'
+                        result += pump_work_signal
 
-        if object_name != '':
-            if signal_type == 'OPEN':
-                valve_opened_signal = 'V_' + object_name + '_open' + '\t' + sr + '.' + var_name + '\n'
-                result += valve_opened_signal
+                    if signal == 'FAULT':
+                        pump_fault_signal = object_name + '_pump_fault' + '\t' + sr + '.' + var_name + '\n'
+                        result += pump_fault_signal
 
-            if signal_type == 'CLOSE':
-                valve_closed_signal = 'V_' + object_name + '_close' + '\t' + sr + '.' + var_name + '\n'
-                result += valve_closed_signal
-    return (result, sr)
-
-def signals_matches_mtr_di(path: str) -> tuple:
-    result = ''
-    with open(path, 'r', encoding='utf-8') as file:
-        var_declarations_string = file.read()
-    declarations = var_declarations_string.split('\n')
-    sr = declarations[0].split(sep=' ')[1]
-
-    for declaration in declarations:
-        object_name_list = re.search(r'N[A-Z]?\d{1,2}(_\d)?', declaration)
-        object_name:str = ''
-        var_name:str = ''
-        signal_type:str = ''
-        if object_name_list:
-            object_name = object_name_list[0]
-            splitted_declaration = declaration.split(sep=':')
-            var_name = re.sub(r'\s', '', splitted_declaration[0])
-            splitted_var_name = var_name.split(sep='_')
-            signal_type = splitted_var_name[len(splitted_var_name) - 1]
-
-        if object_name != '':
-            if signal_type == 'WORK':
-                pump_work_signal = object_name + '_pump_work' + '\t' + sr + '.' + var_name + '\n'
-                result += pump_work_signal
-
-            if signal_type == 'FAULT':
-                pump_fault_signal = object_name + '_pump_fault' + '\t' + sr + '.' + var_name + '\n'
-                result += pump_fault_signal
-
-            if signal_type == 'WAITING':
-                pump_waiting_signal = object_name + '_pump_waiting' + '\t' + sr + '.' + var_name + '\n'
-                result += pump_waiting_signal
-    return (result, sr)
-
-def signals_matches_mtr_do(path: str) -> tuple:
-    result = ''
-    with open(path, 'r', encoding='utf-8') as file:
-        var_declarations_string = file.read()
-    declarations = var_declarations_string.split('\n')
-    sr = declarations[0].split(sep=' ')[1]
-
-    for declaration in declarations:
-        object_name_list = re.search(r'N[A-Z]?\d{1,2}(_\d)?', declaration)
-        object_name:str = ''
-        var_name:str = ''
-        signal_type:str = ''
-        if object_name_list:
-            object_name = object_name_list[0]
-            splitted_declaration = declaration.split(sep=':')
-            var_name = re.sub(r'\s', '', splitted_declaration[0])
-            splitted_var_name = var_name.split(sep='_')
-            signal_type = splitted_var_name[len(splitted_var_name) - 1]
-
-        if object_name != '':
-            if signal_type == 'OFF':
-                pump_setStart_signal = object_name + '_pump_setStart' + '\t' + sr + '.' + var_name + '\n'
-                result += pump_setStart_signal
+                    if signal == 'WAITING':
+                        pump_waiting_signal = object_name + '_pump_waiting' + '\t' + sr + '.' + var_name + '\n'
+                        result += pump_waiting_signal
+            elif signal_type == 'do':
+                if object_name != '':
+                    if signal == 'OFF':
+                        pump_setStart_signal = object_name + '_pump_setStart' + '\t' + sr + '.' + var_name + '\n'
+                        result += pump_setStart_signal
     return (result, sr)
 
 def write_str_to_file(data: str, path: str) -> None:
@@ -215,14 +169,14 @@ def write_signal_to_excel(data: str, path: str, equipment_type: str, signal_type
              'comment': comments, 'modbus': '', 'node': ''})
         df.to_excel(path)
 
-signals_valve_di = signals_matches_valve_di('Files/var_declarations')
+signals_valve_di = signals_matches('Files/var_declarations', 'valve', 'di')
 write_signal_to_excel(signals_valve_di[0], 'Files/valve_di_' + signals_valve_di[1] + '.xlsx', 'valve', 'di')
 
-signals_valve_do = signals_matches_valve_do('Files/var_declarations')
+signals_valve_do = signals_matches('Files/var_declarations', 'valve', 'do')
 write_signal_to_excel(signals_valve_do[0], 'Files/valve_do_' + signals_valve_do[1] + '.xlsx', 'valve', 'do')
 
-signals_mtr_di = signals_matches_mtr_di('Files/var_declarations')
+signals_mtr_di = signals_matches('Files/var_declarations', 'mtr', 'di')
 write_signal_to_excel(signals_mtr_di[0], 'Files/mtr_di_' + signals_mtr_di[1] + '.xlsx', 'mtr', 'di')
 
-signals_mtr_do = signals_matches_mtr_do('Files/var_declarations')
+signals_mtr_do = signals_matches('Files/var_declarations', 'mtr', 'do')
 write_signal_to_excel(signals_mtr_do[0], 'Files/mtr_do_' + signals_mtr_do[1] + '.xlsx', 'mtr', 'do')
