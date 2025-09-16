9a9bad06257716cf25fa1fe00b3fdf325f4db004
cbrton <cbrton0@yandex.ru>
2025-09-16 16:37:55 +0500

    Добавлен файл "requirements.txt".

--- SignalsGenerator.py
+++ SignalsGenerator.py
@@ -10,7 +10,7 @@ def signals_matches(read_path: str, equipment_type: str, signal_type: str) -> tu
 
     if equipment_type == 'valve':
         for declaration in declarations:
-            object_name_list = re.search(r'\d{1,2}[a-z][a-z][a-z][a-z]?\d{1,3}(_\d)?', declaration)     # Поиск названия клапана с помощью регулярки ()
+            object_name_list = re.search(r'\d{1,2}[a-z][a-z][a-z][a-z]?\d{1,3}(_\d)?', declaration)     # Поиск названия клапана с помощью регулярки
             object_name: str = ''
             var_name: str = ''
             signal_desc: str = ''
